b355058ed2a592161c5f7de70789e9daf6980d82
bhavisha550 <bhavishamoghe@gmail.com>
2024-05-31 03:25:55 +0530

    initialORM2

--- springORM/src/main/java/com/spring/orm/Dao/PatientDao.java
+++ springORM/src/main/java/com/spring/orm/Dao/PatientDao.java
@@ -2,14 +2,24 @@ package com.spring.orm.Dao;
 
 import com.spring.orm.entities.Patient;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 public class PatientDao {
 
     private HibernateTemplate hibernateTemplate;
+    @Transactional
     public int insert(Patient patient)
     {
         //insert
         Integer i = (Integer) this.hibernateTemplate.save(patient);
         return i;
+    }
+
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
+
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
     }
 }
--- springORM/src/main/java/org/example/App.java
+++ springORM/src/main/java/org/example/App.java
@@ -1,9 +1,18 @@
 package org.example;
 
-public class App 
+import com.spring.orm.Dao.PatientDao;
+import com.spring.orm.entities.Patient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("ormConfig.xml");
+        PatientDao patientDao = context.getBean("patientDao", PatientDao.class);
+        Patient patient = new Patient(12, "abc", "URI");
+        int r = patientDao.insert(patient);
+        System.out.println(r);
     }
 }
