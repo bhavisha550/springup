11e528c73b885b9e0c3765b5bc5a3663c1122f1a
bhavisha550 <bhavishamoghe@gmail.com>
2024-05-30 16:59:15 +0530

    check

--- springjdbc/src/main/java/dbo/StudentDao.java
+++ springjdbc/src/main/java/dbo/StudentDao.java
@@ -6,7 +6,7 @@ public interface StudentDao{
     public int insert(Student student); //insert
     public int change(Student student); //update
     public int delete(int studentId); //delete
-    public Student getStudent()
+    public Student getStudent(int studentId);
 
 
 }
