9a452b9afbb2ba6b4517fd92142db3569c65be0a
bhavisha550 <bhavishamoghe@gmail.com>
2024-06-01 17:44:41 +0530

    finalORM

--- springORM/src/main/java/com/spring/orm/Dao/PatientDao.java
+++ springORM/src/main/java/com/spring/orm/Dao/PatientDao.java
@@ -4,6 +4,8 @@ import com.spring.orm.entities.Patient;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public class PatientDao {
 
     private HibernateTemplate hibernateTemplate;
@@ -14,7 +16,32 @@ public class PatientDao {
         Integer i = (Integer) this.hibernateTemplate.save(patient);
         return i;
     }
+    //get single data
+    public Patient getPatient(int patientId)
+    {
+        Patient patient = this.hibernateTemplate.get(Patient.class, patientId);
+        return patient;
+    }
+    //get all rows
+    public List<Patient> getAllPatients()
+    {
+        List<Patient> patients = this.hibernateTemplate.loadAll(Patient.class);
+        return patients;
+    }
 
+    //delete
+    @Transactional
+    public void deletePatient(int patientId)
+    {
+        Patient patient = this.hibernateTemplate.get(Patient.class, patientId);
+        this.hibernateTemplate.delete(patient);
+    }
+
+    //update
+    @Transactional
+    public void updatePatient(Patient patient){
+        this.hibernateTemplate.update(patient);
+    }
     public HibernateTemplate getHibernateTemplate() {
         return hibernateTemplate;
     }
